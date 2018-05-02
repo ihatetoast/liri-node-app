23c502d683e278d2bd3344b182f03c42df6c9f78
Katy <ihatetoast@gmail.com>
2018-05-01 21:08:12 -0500

    npm update

--- commands.js
+++ commands.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 const fs = require('fs');
 
 const request = require('request');
@@ -13,11 +13,24 @@ const keys = require('./keys');
 const client = new Twitter(keys.twitter);
 const spotify = new Spotify(keys.spotify);
 const table = new Table({
-  chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
-  , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
-  , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
-  , 'right': '' , 'right-mid': '' , 'middle': ' ' },
-style: { 'padding-left': 0, 'padding-right': 0 }
+  chars: {
+    top: '',
+    'top-mid': '',
+    'top-left': '',
+    'top-right': '',
+    bottom: '',
+    'bottom-mid': '',
+    'bottom-left': '',
+    'bottom-right': '',
+    left: '',
+    'left-mid': '',
+    mid: '',
+    'mid-mid': '',
+    right: '',
+    'right-mid': '',
+    middle: ' '
+  },
+  style: { 'padding-left': 0, 'padding-right': 0 }
 });
 
 //HANDLE LOGGING INFO
@@ -25,78 +38,75 @@ const getDate = () => {
   var nowUgly = Date.now();
   var timePretty = new Date(nowUgly);
   return timePretty;
-}
-//get data from log. 
-//save as array
-//push new data from user into array
-//write to log.txt
-//log the data
+};
 
 const fetchAllLogs = () => {
-  try{
-    var allTheLogs = fs.readFileSync("log.txt", "utf8")
- 
+  try {
+    var allTheLogs = fs.readFileSync('log.txt', 'utf8');
+
     return JSON.parse(allTheLogs);
+  } catch (error) {
+    return [];
   }
-  catch(error){
-    return []
-  }
-}
-const logData = (text) =>{ 
-  fs.writeFileSync('log.txt', JSON.stringify(text))
-}
-const updateLog = (results) =>{
+};
+const logData = text => {
+  fs.writeFileSync('log.txt', JSON.stringify(text));
+};
+const updateLog = results => {
   var currentLogs = fetchAllLogs();
   currentLogs.push(results);
-  logData(currentLogs)
-}  
+  logData(currentLogs);
+};
 
 //LIRI COMMANDS
-const spotifyThisSong = (title) =>{
-  spotify.search({ 
-    type: 'track', 
-    query: title || 'A Town Called Malice'
-  }, 
+const spotifyThisSong = title => {
+  spotify.search(
+    {
+      type: 'track',
+      query: title || 'A Town Called Malice'
+    },
     function(err, data) {
       if (err) {
         return console.log('Error occurred: ' + err);
       }
- 
-  const preview = data.tracks.items[0].preview_url;
-  const artist = data.tracks.items[0].artists[0].name;
-  const album = data.tracks.items[0].artists[0].name;
+
+      const preview = data.tracks.items[0].preview_url;
+      const artist = data.tracks.items[0].artists[0].name;
+      const album = data.tracks.items[0].artists[0].name;
       table.push(
-        {'Title': title || 'A Town Called Malice'},
-        {'Artist': artist || "The Jam"},
-        {'Album': album || ''},
-        {'Preview': preview || ''}
+        { Title: title || 'A Town Called Malice' },
+        { Artist: artist || 'The Jam' },
+        { Album: album || '' },
+        { Preview: preview || '' }
       );
       var spotifyData = {
         time: getDate(),
         Title: title || 'A Town Called Malice',
-        Artist: artist || "The Jam",
+        Artist: artist || 'The Jam',
         Album: album || '',
         Preview: preview || ''
-      }
+      };
       console.log(table.toString());
-      updateLog(spotifyData) ;
-  });
-}
+      updateLog(spotifyData);
+    }
+  );
+};
 
-const movieThis = (name) =>{
-  const url = `http://www.omdbapi.com/?t=${name || 'the castle'}&apikey=6a1aca9d`
+const movieThis = name => {
+  const url = `http://www.omdbapi.com/?t=${name ||
+    'the castle'}&apikey=6a1aca9d`;
   request(url, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       const info = JSON.parse(body);
-      table.push (
-        {'Movie Title': info.Title},
-        {'Release Date': info.Year},
-        {'IMDB Rating': info.Ratings[0].Value},
-        {'Rotten Tomatoes Rating': info.Ratings[1].Value},
-        {'Country(ies)': info.Country },
-        {'Language': info.Language},
-        {'Starring': info.Actors},
-        {'Plot': info.Plot},
+      table.push(
+        { 'Movie Title': info.Title },
+        { 'Release Date': info.Year },
+        { 'IMDB Rating': info.Ratings[0].Value },
+        { 'Rotten Tomatoes Rating': info.Ratings[1].Value },
+        { 'Country(ies)': info.Country },
+        { Language: info.Language },
+        { Starring: info.Actors },
+        { Plot: info.Plot }
       );
       var movieResults = {
         time: getDate(),
@@ -107,47 +117,48 @@ const movieThis = (name) =>{
         'Country(ies)': info.Country,
         Language: info.Language,
         Starring: info.Actors,
-        Plot: info.Plot,
-      }
+        Plot: info.Plot
+      };
       console.log(table.toString());
-      updateLog(movieResults)    
+      updateLog(movieResults);
     }
   });
-}
+};
 
 const myTweets = () => {
-  client.get('statuses/user_timeline.json?screen_name=i_hate_toast&count=20', function(error, tweets, response){
-    if(error) throw error;
-    for (var tweet in tweets) {
-      table.push (
-      {'tweet': tweets[tweet]["text"]},
-      {'on': tweets[tweet]["created_at"]},
-      { '' : "🐦 🐦 🐦 🐦 🐦 🐦 🐦 🐦"}
-      )
+  client.get(
+    'statuses/user_timeline.json?screen_name=i_hate_toast&count=20',
+    function(error, tweets, response) {
+      if (error) throw error;
+      for (var tweet in tweets) {
+        table.push(
+          { tweet: tweets[tweet]['text'] },
+          { on: tweets[tweet]['created_at'] },
+          { '': '🐦 🐦 🐦 🐦 🐦 🐦 🐦 🐦' }
+        );
+      }
+      var tweetLog = {
+        time: getDate(),
+        text: 'Tweets retrieved'
+      };
+      console.log(table.toString());
+      updateLog(tweetLog);
     }
-    var tweetLog = {
-      time: getDate(),
-      text: "Tweets retrieved"
-    }
-    console.log(table.toString());
-    updateLog(tweetLog);
-  })
-}
+  );
+};
 const randomCommandom = () => {
-  fs.readFile("random.txt", "utf8", function(error, text) {
-    if (error)
-      throw error;
+  fs.readFile('random.txt', 'utf8', function(error, text) {
+    if (error) throw error;
     var args = text.split(',');
     const song = args[1];
     const doThis = args[0];
     spotifyThisSong(song);
   });
-}
+};
 const showLog = () => {
   var logTxt = fetchAllLogs();
   console.log(logTxt);
-}
-
+};
 
 module.exports = {
   spotifyThisSong,
@@ -155,4 +166,4 @@ module.exports = {
   myTweets,
   randomCommandom,
   showLog
-}
+};
